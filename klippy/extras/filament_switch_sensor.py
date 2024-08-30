83bf2b5782540ba46da4057e9011cca744ffbce2
zhc <310117311@qq.com>
2024-08-30 22:01:07 +0800

    Modify The filament detected state is incorrect after a save and restart

--- klippy/extras/filament_switch_sensor.py
+++ klippy/extras/filament_switch_sensor.py
@@ -27,7 +27,7 @@ class RunoutHelper:
         self.event_delay = config.getfloat('event_delay', 3., above=0.)
         # Internal state
         self.min_event_systime = self.reactor.NEVER
-        self.filament_present = False
+        self.filament_present = True
         self.sensor_enabled = True
         # Register commands and event handlers
         self.printer.register_event_handler("klippy:ready", self._handle_ready)
@@ -74,6 +74,7 @@ class RunoutHelper:
     def note_filament_present(self, is_filament_present):
         if is_filament_present == self.filament_present:
             return
+        logging.info("is_filament_present: %d",is_filament_present)
         self.filament_present = is_filament_present
         eventtime = self.reactor.monotonic()
         if eventtime < self.min_event_systime or not self.sensor_enabled:
